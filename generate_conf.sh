fd3da726eebab88fd362c20a8a7ae3097a0a5384
Ivan Malison <imalison@yelp.com>
2013-04-08 17:14:02 -0700

    oops.

--- generate_conf.sh
+++ generate_conf.sh
@@ -3,16 +3,17 @@ source themes/default.sh &>/dev/null
 
 function writeTmuxConf() {
     echo 'set-option -g status on
+bind-key @ command-prompt -p "create pane from:"  "join-pane -s ':%%'"
 set-option -g status-interval 2
 set-option -g status-utf8 on
 set-option -g status-justify "centre"
 set-option -g status-left-length 60
 set-option -g status-right-length 90
-set-option -g status-left "#('`pwd`'/powerline.sh left)"
+set-option -g status-left "#('`pwd`'/powerline.s
 set-option -g status-right "#('`pwd`'/powerline.sh right)"
 set-option -g status-bg colour'$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR'
 set-option -g status-fg colour'$TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR > ~/.tmux.conf
-bind-key @ command-prompt -p "create pane from:"  "join-pane -s ':%%'"
+
 echo '
 ' >> ~/.zshrc
 echo 'PROMPT="$PROMPT"''$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")' >> ~/.prompt
